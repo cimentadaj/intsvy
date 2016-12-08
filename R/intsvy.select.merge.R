cb02fce9f3371154fe006ca10c808ceec3620b76
Jorge Cimentada <cimentadaj@gmail.com>
2016-12-08 16:26:46 +0100

    Update intsvy.select.merge.R

--- R/intsvy.select.merge.R
+++ R/intsvy.select.merge.R
@@ -56,6 +56,8 @@ function(folder=getwd(), countries, student=c(), home, school, teacher, use.labe
   nchar(y)+config$input$cnt_part[1], nchar(y)+config$input$cnt_part[2])==tolower(x)])) 
   # no blanks, no home instrument, otherwise delete, see 4g function
   
+  files.select <- sapply(files.all, function(x) Filter(function(var) length(var) != 0, x))          
+ 
   # Remove cases for no home instruments
   # only if home is specified
   if (!missing(home)) {
